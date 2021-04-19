5a294d4a2996215036b283a25c0e4239659c86ba
koftov <ahronlu@gmail.com>
2021-04-19 23:33:01 +0300

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>Punk Show - About</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>About</h1>
+      <p>This is an app to find punk shows</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
