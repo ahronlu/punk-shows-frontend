203bc1a1b3758cafe89c20b611601d7152221ba6
koftov <ahronlu@gmail.com>
2021-04-20 00:11:12 +0300

    404 Page

--- pages/about.js
+++ pages/about.js
@@ -5,10 +5,6 @@ import Layout from "../components/Layout";
 export default function About() {
   return (
     <Layout title="About Punk Shows">
-      <Head>
-        <title>Punk Show - About</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <h1>About</h1>
       <p>This is an app to find punk shows</p>
       <p>Version: 1.0.0</p>
